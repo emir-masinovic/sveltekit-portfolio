9d831c171c25372fbb5e5d66042cd87628145cb6
Emir Masinovic <emir3710549@gmail.com>
2025-12-03 15:23:15 +0300

    adapter changed to node. sveltekit and prettier updated

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
 // import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-auto';
+
 
 export default {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true,
-			host: '0.0.0.0'
+			// pages: 'build',
+			// assets: 'build',
+			// fallback: undefined,
+			// precompress: false,
+			// strict: true,
+			// host: '0.0.0.0'
 		})
 	}
 };
